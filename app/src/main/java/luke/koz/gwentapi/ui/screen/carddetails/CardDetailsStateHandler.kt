95f7588bd8f8097231505f947c5ac935421589f5
Luke <lucjan.kozielecki@gmail.com>
2025-05-17 19:45:24 +0200

    refactor(CardStateHandling): reorganize state handlers and cleanup
    - move state handler used by CardDetailsStateHandler form `luke.koz.gwentapi.ui.screen.components.cardstate` to `luke.koz.gwentapi.ui.screen.cardgallery`
    - move state handler used by `CardDetailStateHandler` form `luke.koz.gwentapi.ui.screen.components.cardstate` to `luke.koz.gwentapi.ui.screen.carddetails`
    - rename moved functions to `CardGalleryStateHandlerInternal` and `CardDetailsStateHandlerInternal` respectively
    - delete empty `CardStateHandler` file inside `luke.koz.gwentapi.ui.screen.components.cardstate`

--- app/src/main/java/luke/koz/gwentapi/ui/screen/carddetails/CardDetailsStateHandler.kt
+++ app/src/main/java/luke/koz/gwentapi/ui/screen/carddetails/CardDetailsStateHandler.kt
@@ -1,16 +1,32 @@
 package luke.koz.gwentapi.ui.screen.carddetails
 
 import androidx.compose.runtime.Composable
-import luke.koz.gwentapi.ui.screen.components.cardstate.CardDetailsStateHandlerOuter
-import luke.koz.gwentapi.ui.screen.components.cardstate.CardStateHandler
+import luke.koz.gwentapi.domain.model.CardDetailsEntry
+import luke.koz.gwentapi.ui.screen.components.cardstate.ErrorMessage
 import luke.koz.gwentapi.ui.state.CardDetailsState
 
 @Composable
 fun CardDetailsStateHandler(state: CardDetailsState, onCardClick: (Int) -> Unit) {
-    CardDetailsStateHandlerOuter(
+    CardDetailsStateHandlerInternal(
         state = state,
         onEmpty = { /* Empty composable */ },
         onLoading = { /* Empty composable */ },
         onSuccess = { cards -> CardItemDetails(card = cards.first(), onCardClick) }
     )
+}
+
+@Composable
+private fun CardDetailsStateHandlerInternal(
+    state: CardDetailsState,
+    onEmpty: @Composable () -> Unit,
+    onLoading: @Composable () -> Unit,
+    onSuccess: @Composable (List<CardDetailsEntry>) -> Unit,
+    onError: @Composable (String) -> Unit = { ErrorMessage(message = it) }
+) {
+    when (state) {
+        is CardDetailsState.Empty -> onEmpty()
+        is CardDetailsState.Loading -> onLoading()
+        is CardDetailsState.Success -> onSuccess(state.cards)
+        is CardDetailsState.Error -> onError(state.message)
+    }
 }
--- app/src/main/java/luke/koz/gwentapi/ui/screen/cardgallery/CardGalleryStateHandler.kt
+++ app/src/main/java/luke/koz/gwentapi/ui/screen/cardgallery/CardGalleryStateHandler.kt
@@ -2,17 +2,34 @@ package luke.koz.gwentapi.ui.screen.cardgallery
 
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import luke.koz.gwentapi.domain.model.CardGalleryEntry
 import luke.koz.gwentapi.ui.screen.components.cardlist.CardList
 import luke.koz.gwentapi.ui.screen.components.cardstate.EmptyState
-import luke.koz.gwentapi.ui.screen.components.cardstate.CardStateHandler
+import luke.koz.gwentapi.ui.screen.components.cardstate.ErrorMessage
 import luke.koz.gwentapi.ui.state.CardState
 
 @Composable
 fun CardGalleryStateHandler(state: CardState, onCardClick : (Int) -> Unit) {
-    CardStateHandler(
+    CardGalleryStateHandlerInternal(
         state = state,
         onEmpty = { EmptyState() },
         onLoading = { CircularProgressIndicator() },
         onSuccess = { cards -> CardList(cards = cards, onCardClick) }
     )
+}
+
+@Composable
+private fun CardGalleryStateHandlerInternal(
+    state: CardState,
+    onEmpty: @Composable () -> Unit,
+    onLoading: @Composable () -> Unit,
+    onSuccess: @Composable (List<CardGalleryEntry>) -> Unit,
+    onError: @Composable (String) -> Unit = { ErrorMessage(message = it) }
+) {
+    when (state) {
+        is CardState.Empty -> onEmpty()
+        is CardState.Loading -> onLoading()
+        is CardState.Success -> onSuccess(state.cards)
+        is CardState.Error -> onError(state.message)
+    }
 }
