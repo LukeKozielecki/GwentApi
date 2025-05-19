452a398807401b849265d7fdcc3d8d271ab3e8cc
Luke <lucjan.kozielecki@gmail.com>
2025-05-19 17:27:29 +0200

    fix(auth): clear back stack after auth success
    - Remove AuthDestination from back stack using popUpTo
    - Ensure user cannot return to Auth screen after login
    - Keep start destination, the CardGalleryDestination, as only screen in the navigation stack

--- app/src/main/java/luke/koz/gwentapi/navigation/NavigationHost.kt
+++ app/src/main/java/luke/koz/gwentapi/navigation/NavigationHost.kt
@@ -7,24 +7,22 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.toRoute
 import kotlinx.serialization.Serializable
-import luke.koz.gwentapi.ui.viewmodel.SearchViewModel
-import luke.koz.gwentapi.ui.screen.carddetails.CardDetailScreen
-import luke.koz.gwentapi.ui.screen.cardgallery.CardGalleryScreen
 import luke.koz.gwentapi.di.provideSearchGalleryViewModel
 import luke.koz.gwentapi.ui.screen.auth.AuthScreen
+import luke.koz.gwentapi.ui.screen.carddetails.CardDetailScreen
+import luke.koz.gwentapi.ui.screen.cardgallery.CardGalleryScreen
 import luke.koz.gwentapi.ui.screen.search.SearchScreen
+import luke.koz.gwentapi.ui.viewmodel.SearchViewModel
 
 @Serializable
 object CardGalleryDestination
@@ -50,10 +48,16 @@ fun NavigationHost(
         exitTransition = { fadeOut(animationSpec = tween(200, delayMillis = 30)) },
         modifier = modifier.background(MaterialTheme.colorScheme.background)
     ) {
-        //todo this probably is added to backstack, it should not be, fix later
         composable<AuthDestination> {
             AuthScreen(
-                onAuthSuccess = { navController.navigate(CardGalleryDestination) },
+                onAuthSuccess = {
+                    navController.navigate(CardGalleryDestination) {
+                        popUpTo(navController.graph.startDestinationId) {
+                            inclusive = true
+                        }
+                        launchSingleTop = true
+                    }
+                },
                 navController = navController,
                 modifier = Modifier.fillMaxSize()
             )
