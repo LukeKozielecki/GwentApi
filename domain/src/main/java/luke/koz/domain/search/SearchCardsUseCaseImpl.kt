ce4ac702707771ad06f6752b15395f373d35a273
Luke <lucjan.kozielecki@gmail.com>
2025-06-27 18:30:35 +0200

    fix: `SearchCardsUseCaseImpl` `invoke` not emitting when query is blank
    desc: previous implementation prevented flow emission when query was blank, which would keep UI in SearchState.Loading (within `SearchViewModel` `getCardByQuery()`)
    
    - `SearchCardsUseCaseImpl` `invoke` body:
      - remove `.filter { it.isNotBlank() }`
      - add conditional block if query is blank emit empty else

--- domain/src/main/java/luke/koz/domain/search/SearchCardsUseCaseImpl.kt
+++ domain/src/main/java/luke/koz/domain/search/SearchCardsUseCaseImpl.kt
@@ -4,7 +4,6 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.distinctUntilChanged
-import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flowOf
 import luke.koz.domain.model.CardGalleryEntry
@@ -13,14 +12,21 @@ import luke.koz.domain.repository.CardGalleryRepository
 class SearchCardsUseCaseImpl(
     private val cardGalleryRepository: CardGalleryRepository
 ) : SearchCardsUseCase {
+
+    private val _debounceTimeMillis : Long = 500L
+
     @OptIn(ExperimentalCoroutinesApi::class)
     override operator fun invoke(query: String): Flow<List<CardGalleryEntry>> {
         return flowOf(query)
             .distinctUntilChanged()
-            .filter { it.isNotBlank() }
             .flatMapLatest { searchQuery ->
-                delay(500)
-                cardGalleryRepository.getCardByQuery(searchQuery)
+                if (searchQuery.isBlank()) {
+                    delay(_debounceTimeMillis) //this is here to provide consistent UX when transitioning from non-empty query to empty query
+                    flowOf(emptyList())
+                } else {
+                    delay(_debounceTimeMillis)
+                    cardGalleryRepository.getCardByQuery(searchQuery)
+                }
             }
     }
 }
