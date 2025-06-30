7be46450ae4b87a2ce69d4389a77fa58591e15df
Luke <lucjan.kozielecki@gmail.com>
2025-06-30 22:11:11 +0200

    docs: add documentation for `RenderSearchState` composable function

--- feature/search/src/main/java/luke/koz/search/components/RenderSearchState.kt
+++ feature/search/src/main/java/luke/koz/search/components/RenderSearchState.kt
@@ -9,6 +9,21 @@ import luke.koz.presentation.statusscreen.LoadingStatusScreen
 import luke.koz.presentation.statusscreen.NoDataStatusScreen
 import luke.koz.presentation.statusscreen.SuccessStatusScreen
 
+/**
+ * Renders the appropriate UI based on [SearchState].
+ *
+ * This composable acts as a state-to-UI mapper for the search screen, displaying
+ * different result (idle, loading, empty, success, or error) depending on the
+ * received [searchState]. It delegates the actual content rendering for each
+ * state to specialized status functions.
+ *
+ * @param searchState The current state of the search operation. Determines which UI to display.
+ * @param onCardClick A callback function invoked when a card item in the success state is clicked.
+ * @param imageLoader An [ImageLoader] instance used for loading images within the displayed content
+ * for the card list in the success state.
+ * @param combinedResults The list of [CardGalleryEntry] items to display when the [searchState]
+ * is [SearchState.Success]. These results are expected to be already filtered.
+ */
 @Composable
 internal fun RenderSearchState(
     searchState: SearchState,
