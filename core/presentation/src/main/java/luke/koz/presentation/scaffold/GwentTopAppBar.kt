b9ef9efa9e88186fe1019976c11e09e87f065f1f
Luke <lucjan.kozielecki@gmail.com>
2025-07-01 17:25:50 +0200

    refactor: extract title block from `GwentTopAppBar` to separate composable

--- core/presentation/src/main/java/luke/koz/presentation/scaffold/GwentTopAppBar.kt
+++ core/presentation/src/main/java/luke/koz/presentation/scaffold/GwentTopAppBar.kt
@@ -1,25 +1,15 @@
 package luke.koz.presentation.scaffold
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import luke.koz.presentation.R
+import luke.koz.presentation.scaffold.components.CoreTopAppBarTitle
 import luke.koz.presentation.theme.GwentApiTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -30,20 +20,7 @@ fun GwentTopAppBar(
 ) {
     TopAppBar(
         title = {
-            Row(
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Image(
-                    painter = painterResource(id = R.drawable.gwent_one_api_favicon_96x96),
-                    contentDescription = "App Logo",
-                    modifier = Modifier.size(32.dp)
-                )
-                Text(
-                    text = "Gwent API",
-                    style = MaterialTheme.typography.titleMedium,
-                    modifier = Modifier.padding(start = 8.dp)
-                )
-            }
+            CoreTopAppBarTitle()
         },
         actions = {
             IconButton(onClick = onSearchClicked) {
---
+++ core/presentation/src/main/java/luke/koz/presentation/scaffold/components/CoreAppTopBarTitle.kt
@@ -0,0 +1,42 @@
+package luke.koz.presentation.scaffold.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import luke.koz.presentation.R
+import luke.koz.presentation.theme.GwentApiTheme
+
+@Composable
+fun CoreTopAppBarTitle(modifier: Modifier = Modifier) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.gwent_one_api_favicon_96x96),
+            contentDescription = "App Logo",
+            modifier = Modifier.size(32.dp)
+        )
+        Text(
+            text = "Gwent API",
+            style = MaterialTheme.typography.titleMedium,
+            modifier = Modifier.padding(start = 8.dp)
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun CoreTopAppBarTitlePrev() {
+    GwentApiTheme {
+        CoreTopAppBarTitle()
+    }
+}
